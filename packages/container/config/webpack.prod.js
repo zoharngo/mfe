f4786f8b99ded988b3bd0a7e1dfded78c05954f1
Zohar Nyego <zohar@ox.security>
2024-03-15 23:21:35 +0200

    Add HtmlWebpackPlugin to webpack.common.js and remove it from webpack.dev.js

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge');
+const ModuelFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+const domain = process.env.PRODUCTION_DOMAIN;
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+        publicPath: '/container/latest/'
+    },
+    plugins: [
+        new ModuelFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+            },
+            shared: packageJson.dependencies
+        }),
+    ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge');
+const ModuelFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
+  },
+  plugins: [
+    new ModuelFederationPlugin({
+      name: 'martketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
