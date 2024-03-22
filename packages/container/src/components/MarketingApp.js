029fc7931619f54df81191ee015d3b7d977039b4
Zohar Nyego <zoharngo@gmail.com>
2024-03-22 19:05:00 +0200

    Refactor navigation in MarketingApp.js and App.js

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,21 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
-
+  const history = useHistory();
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: (location) => {
+        const { pathname: nextPathname } = location; // this is the path that the Marketing App is trying to navigate to
+        const { pathname: currentPathname } = history.location; // this is the current path that the Container App is on
+        if (currentPathname !== nextPathname) {
+          history.push(nextPathname); // this is how we navigate to the path that the Marketing App is trying to navigate to
+        }
+      },
+    });
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
+const mount = (el, { onNavigate, defualtHistory }) => {
+
+  const history = defualtHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === 'development') {
   const el = document.querySelector('#_marketing-dev-root');
   if (el) {
-    mount(el);
+    mount(el, {
+      defualtHistory: createBrowserHistory(),
+    });
   }
 }
 
