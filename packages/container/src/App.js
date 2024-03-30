209b553a26ee4393c883c2a39f312286891ef2e1
Zohar Nyego <zoharngo@gmail.com>
2024-03-30 21:40:35 +0300

    Add lazy loading and suspense for code splitting

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
-import MarketingApp from './components/MarketingApp';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-import AuthApp from './components/AuthApp';
+import Progress from './components/Progress';
 //import CardsGame from './components/CardsGame';
+
+const MarketingApp = lazy(() => import('./components/MarketingApp'));
+const AuthApp = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
@@ -16,10 +18,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route path='/auth' component={AuthApp} />
-            <Route path='/' component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path='/auth' component={AuthApp} />
+              <Route path='/' component={MarketingApp} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
