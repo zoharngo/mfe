5c103ddaf809c64d912dabeab2aa31602d949179
Zohar Nyego <zoharngo@gmail.com>
2024-03-30 19:45:53 +0300

    Add SignIn and SignUp components to App.js

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import { Router, Route, Switch } from 'react-router-dom';
+
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
